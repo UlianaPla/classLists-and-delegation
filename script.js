da57f89d4a75741c0a4ebd13682ddc85331fd758
Ulia <lyanusia@gmail.com>
2022-01-27 15:40:14 +0200

    classLists and events delegation

---
+++ script.js
@@ -0,0 +1,41 @@
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+//console.log(btns[0].classList.length);
+//console.log(btns[0].classList.item(1));
+console.log(btns[1].classList.add("red"));
+//console.log(btns[0].classList.remove("blue"));
+//console.log(btns[0].classList.toggle("blue"));
+
+// if(btns[1].classList.contains('red')){
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if(!btns[1].classList.contains('red')){
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    btns[1].classList.toggle('red');
+});
+
+wrapper.addEventListener('click', (event) => {
+    //if(event.target && event.target.tagName == "BUTTON"){
+    if(event.target && event.target.matches("button.red")){ // matches любят использовать в GOOGLE, более продвинутый метод
+        console.log("HELLO");
+    }
+});
+
+//Если же не использовать делегирование на обертку, то можно описать так:
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('hello');
+//     });
+// });
+//Но в таком случае новодобавленные кнопки не будут обрабатывать событие клика.
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
+
